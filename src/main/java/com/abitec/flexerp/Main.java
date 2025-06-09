8e4e15fae06ea0a05f3737ed76038b7ec3807aba
Andre Luiz Gomes de Moraes <agomesmoraes@uol.com.br>
2025-06-09 17:07:55 -0300

    Primeiro commit - estrutura inicial ERP

---
+++ src/main/java/com/abitec/flexerp/Main.java
@@ -0,0 +1,8 @@
+package com.abitec.flexerp;
+
+public class Main {
+    public static void main(String[] args) {
+        Empresa empresa = new Empresa("FlexERP Solutions", "12.345.678/0001-99");
+        empresa.imprimirDados();
+    }
+}
